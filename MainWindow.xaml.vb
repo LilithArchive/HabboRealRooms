5e776e5102a16d812e4afbe13a17f0db0d9cefc4
Lilith <44578053+LilithRainbows@users.noreply.github.com>
2021-06-08 22:09:45 -0300

    Fix typo

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -11,7 +11,7 @@ Class MainWindow
         If BlockButton.Content.ToString.StartsWith("Bloquear") Then
             Try
                 If IO.File.Exists(GetClientPath() & "\HabboAir_ORIGINAL.swf") = False Then
-                    BackupOriginalClient()
+                    RestoreOriginalClient()
                 End If
                 CopyModdedClient()
             Catch
